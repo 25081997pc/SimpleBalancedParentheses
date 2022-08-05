190af02b0706cdbf4290f1a7e7c52ec711174d1f
25081997pc <chavanpooja2508@gmail.com>
2022-08-05 14:10:00 +0530

    Balanced Parentheses

---
+++ SimpleBalancedParentheses/Program.cs
@@ -0,0 +1,25 @@
+﻿using SimpleBalancedParentheses;
+
+namespace SimpleBalancedParenthesis
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Expression : ");
+            string expression = Console.ReadLine();
+            StackParenthesis stackParenthesis = new StackParenthesis();
+
+            if(stackParenthesis.IsParenthesis(expression))
+            {
+                Console.WriteLine("Balanced Parenthesis");
+            }
+            else
+            {
+                Console.WriteLine("Not Balanced Parenthesis");
+            }
+
+            
+        }
+    }
+}
---
+++ SimpleBalancedParentheses/StackParenthesis.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleBalancedParentheses
+{
+    public class StackParenthesis
+    {
+        public bool IsParenthesis(String str)
+        {
+            Stack<char> stack = new Stack<char>();
+            for(int i=0;i<str.Length;i++)
+            {
+                char c = str[i];
+                if(c == '[' || c == '(' || c == '{')
+                {
+                    stack.Push(c);
+                }
+                else if (c == ']')
+                {
+                    if(stack.Count == 0|| stack.Pop() != '[')
+                    {
+                        return false;
+                    }
+                }
+                else if(c == ')')
+                {
+                    if(stack.Count == 0|| stack.Pop()!='{')
+                    {
+                        return false;
+                    }
+                }
+            }
+            return stack.Count == 0;
+        }
+
+    }
+}
